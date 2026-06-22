5c6f9e6ad640303bcfa92fc4b012004a52e835e8
ofeklevi26 <115495713+ofeklevi26@users.noreply.github.com>
2026-06-22 22:49:48 +0300

    added the heuristic calculation

--- hw2-files-animation/submission.py
+++ hw2-files-animation/submission.py
@@ -22,7 +22,71 @@ TIE_BREAKING_ORDER = [
 
 # TODO: section a : 3
 def smart_heuristic(env: WarehouseEnv, robot_id: int):
-    pass
+    robot = env.get_robot(robot_id)
+    opponent = env.get_robot((robot_id + 1) % 2)
+
+    # ScoreDiff(s, r)
+    score_diff = robot.credit - opponent.credit
+
+    def package_reward(package):
+        # Reward(p) = 3 * Manhattan(origin(p), destination(p))
+        return 3 * manhattan_distance(package.position, package.destination)
+
+    def route_dist(package):
+        # If the robot is carrying a package, it only needs to reach its destination.
+        if robot.package is not None:
+            return manhattan_distance(robot.position, package.destination)
+
+        # If the robot is not carrying, it needs to reach the package and then deliver it.
+        return (
+            manhattan_distance(robot.position, package.position)
+            + manhattan_distance(package.position, package.destination)
+        )
+
+    # Choose p*
+    if robot.package is not None:
+        selected_package = robot.package
+    else:
+        active_packages = [
+            package for package in env.packages[0:2]
+            if package.on_board
+        ]
+
+        # Defensive case. In the normal environment there should be active packages,
+        # but this avoids a crash if something unusual happens.
+        if len(active_packages) == 0:
+            return 0.5 * score_diff
+
+        selected_package = max(
+            active_packages,
+            key=lambda package: package_reward(package) - route_dist(package)
+        )
+
+    # PackageUtility(s, r) = Reward(p*) - RouteDist(s, r, p*)
+    selected_route_dist = route_dist(selected_package)
+    package_utility = package_reward(selected_package) - selected_route_dist
+
+    # BatteryShortage(s, r)
+    battery_shortage = max(0, selected_route_dist - robot.battery)
+
+    # ChargePenalty(s, r)
+    # The charger is relevant only when there is not enough battery.
+    if battery_shortage > 0:
+        charge_dist = min(
+            manhattan_distance(robot.position, charge_station.position)
+            for charge_station in env.charge_stations
+        )
+        charge_penalty = charge_dist
+    else:
+        charge_penalty = 0
+
+    # EnergyRisk(s, r)
+    energy_risk = battery_shortage + 0.5 * charge_penalty
+
+    # Final heuristic:
+    # h(s,r) = 0.5*ScoreDiff + 3*PackageUtility - 4*EnergyRisk
+    return (0.5 * score_diff + 3 * package_utility - 4 * energy_risk
+    )
 
 
 # TODO: section b : fixed-depth helper for deterministic grading
